672eacc8aa143c963c3a9a88f1f5a1497b608f50
Suhail Moeen <suhailmmoeen@gmail.com>
2024-08-03 13:47:47 -0400

    Updating styling changes

--- TerraHacks/terrahacks-2024/src/App.js
+++ TerraHacks/terrahacks-2024/src/App.js
@@ -1,18 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Classcomp from './components/Classcomp';
 import Functionalcomp from './components/Functionalcomp';
+import Home from './components/pages/Home.js';
+import Model from './components/pages/Model.js';
+import Solution from './components/pages/Solution.js';
 
 function App() {
   return (
     <div className="App">
       <div className="Header">
-        <BrowserRouter>
-          <Navbar />
-        </BrowserRouter>
       </div>
+      <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/model" element={<Model />} />
+            <Route path="/solution" element={<Solution />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
