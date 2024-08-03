4680683f03c37ebe01962ad33bb2695f026f7963
Suhail Moeen <suhailmmoeen@gmail.com>
2024-08-03 12:06:23 -0400

    Updates to Navbar

--- TerraHacks/terrahacks-2024/src/App.js
+++ TerraHacks/terrahacks-2024/src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-        <Functionalcomp name="Fertility Prediction Model"/>
         <BrowserRouter>
           <Navbar />
         </BrowserRouter>
